61847c059619c128abb047075485588d068aee3b
manuelnelson <elnels@gmail.com>
2017-09-07 11:00:16 -0400

    update db connection

--- src/server/db.js
+++ src/server/db.js
@@ -4,8 +4,9 @@ import SeedDdt from './seed/dttType';
 
 export default callback => {
 	mongoose.Promise = require('bluebird');
-	mongoose.connect('mongodb://localhost/boilerplate');
-	var db = mongoose.connection;
+	let mongoUri = process.env.MONGODB_URI || process.env.MONGO_HOST;
+	mongoose.connect(mongoUri);
+	let db = mongoose.connection;
 	db.on('error', console.error.bind(console, 'connection error:'));
 	db.once('open', () => {
 		//check update scripts
